7a9e3100124d63b7eefc756205a94b4f89dc77b3
Deep Shah <deep@makkajai.com>
2018-03-14 22:44:24 +0530

    Show how the bug gets reproduced in the Android CleverTap SDK <Deep>

--- app/src/main/java/com/clevertap/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/clevertap/myfirstapplication/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
 
     String fcmRegId = "";
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,39 +44,8 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
-
-
-
-
-        try {
-            CleverTapAPI.setDebugLevel(3);
-            cleverTap = CleverTapAPI.getInstance(getApplicationContext());
-
-        } catch (CleverTapMetaDataNotFoundException e) {
-            // thrown if you haven't specified your CleverTap Account ID or Token in your AndroidManifest.xml
-        } catch (CleverTapPermissionsNotSatisfied e) {
-            // thrown if you haven’t requested the required permissions in your AndroidManifest.xml
-        }
-
-
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         fcmRegId = FirebaseInstanceId.getInstance().getToken();
         Log.e("push", FirebaseInstanceId.getInstance().getToken() + " hello");
-
 
         clickListeners();
 
@@ -85,73 +53,54 @@ public class MainActivity extends AppCompatActivity {
 
     private void clickListeners() {
 
-
-
-        update_user_profile_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                HashMap<String, Object> profileUpdate = new HashMap<String, Object>();
-                profileUpdate.put("Name", "Jack Montana");                   // String or number
-                profileUpdate.put("Email", "jack123456@gmail.com");
-                profileUpdate.put("Identity", 91026032);             // Email address of the user
-                profileUpdate.put("Phone", "9920879978");                 // Phone (with the country code, starting with +)
-                profileUpdate.put("Gender", "M");                           // Can be either M or F
-                profileUpdate.put("Employed", "Y");                         // Can be either Y or N
-                profileUpdate.put("Education", "Graduate");                 // Can be either Graduate, College or School
-                profileUpdate.put("Married", "Y");                          // Can be either Y or N
-                profileUpdate.put("DOB", new Date());                       // Date of Birth. Set the Date object to the appropriate value first
-                profileUpdate.put("Age", 28);                               // Not required if DOB is set
-                profileUpdate.put("Tz", "Asia/Kolkata");
-
-                cleverTap.profile.push(profileUpdate);
-
-
-            }
-        });
-
-
-
-
-
-
         create_event_without_property_novel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try {
+                    CleverTapAPI.setDebugLevel(1277182231);
+                    cleverTap = CleverTapAPI.getInstance(getApplicationContext());
 
-                // To set a multi-value property
-                ArrayList<String> stuff = new ArrayList<String>();
-                stuff.add("bag new 99");
-                stuff.add("bag new 100");
-                cleverTap.profile.addMultiValuesForKey("TestNew", stuff);
+                    HashMap<String, Object> profileUpdate = new HashMap<String, Object>();
+                    profileUpdate.put("Name", "Jack Montana");                   // String or number
+                    profileUpdate.put("Customer Type", "SUBSCRIBER");
+                    profileUpdate.put("Email", "deep+c7@makkajai.com");
+
+                    cleverTap.profile.push(profileUpdate);
+                    cleverTap.profile.addMultiValueForKey("Apps", "Monster Math 1");
+
+                } catch (CleverTapMetaDataNotFoundException | CleverTapPermissionsNotSatisfied e) {
+                    // thrown if you haven't specified your CleverTap Account ID or Token in your AndroidManifest.xml
+                }
+
 
             }
         });
-
-
 
         product_viewed_with_property.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try {
+                    CleverTapAPI.setDebugLevel(1277182231);
+                    cleverTap = CleverTapAPI.getInstance(getApplicationContext());
 
-                ArrayList<String> stuff = new ArrayList<String>();
-                stuff.add("bag new 2");
-                stuff.add("bag new 1234567");
-                cleverTap.profile.addMultiValuesForKey("TestNew", stuff);
+                    HashMap<String, Object> profileUpdate = new HashMap<String, Object>();
+                    profileUpdate.put("Name", "Jack Montana");                   // String or number
+                    profileUpdate.put("Customer Type", "SUBSCRIBER");
+                    profileUpdate.put("Email", "deep+c7@makkajai.com");
+
+                    cleverTap.profile.push(profileUpdate);
+                    cleverTap.profile.addMultiValueForKey("Apps", "Monster Math 2");
+                } catch (CleverTapMetaDataNotFoundException | CleverTapPermissionsNotSatisfied e) {
+                    // thrown if you haven't specified your CleverTap Account ID or Token in your AndroidManifest.xml
+                }
 
             }
         });
-
-
-
     }
 
     private void init() {
-
-        update_user_profile_button = (Button) findViewById(R.id.update_user_profile_button);
         create_event_without_property_novel = (Button) findViewById(R.id.create_event_without_property_novel);
         product_viewed_with_property = (Button) findViewById(R.id.product_viewed_with_property);
-
-
     }
 
 
